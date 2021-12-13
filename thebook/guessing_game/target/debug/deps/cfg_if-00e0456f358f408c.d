c8dd6521dea903e9b62e5892f8bd91030b189098
Tuan Pham <pat.ptit@gmail.com>
2021-12-14 00:55:00 +0700

    Update .gitignore

--- thebook/guessing_game/target/debug/deps/cfg_if-00e0456f358f408c.d
+++
@@ -1,7 +0,0 @@
-/Users/ali132836/Learning/rust/learning-rust/thebook/guessing_game/target/debug/deps/cfg_if-00e0456f358f408c.rmeta: /Users/ali132836/.cargo/registry/src/github.com-1ecc6299db9ec823/cfg-if-1.0.0/src/lib.rs
-
-/Users/ali132836/Learning/rust/learning-rust/thebook/guessing_game/target/debug/deps/libcfg_if-00e0456f358f408c.rlib: /Users/ali132836/.cargo/registry/src/github.com-1ecc6299db9ec823/cfg-if-1.0.0/src/lib.rs
-
-/Users/ali132836/Learning/rust/learning-rust/thebook/guessing_game/target/debug/deps/cfg_if-00e0456f358f408c.d: /Users/ali132836/.cargo/registry/src/github.com-1ecc6299db9ec823/cfg-if-1.0.0/src/lib.rs
-
-/Users/ali132836/.cargo/registry/src/github.com-1ecc6299db9ec823/cfg-if-1.0.0/src/lib.rs:
--- thebook/guessing_game/target/debug/guessing_game.d
+++
@@ -1,1 +0,0 @@
-/Users/ali132836/Learning/rust/learning-rust/thebook/guessing_game/target/debug/guessing_game: /Users/ali132836/Learning/rust/learning-rust/thebook/guessing_game/src/main.rs
