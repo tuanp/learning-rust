20a6d1f9a2894943e29eb96a1114374d9ad15104
Tuan Pham <pat.ptit@gmail.com>
2021-12-30 02:09:58 +0700

    Add unit tests for ch5

--- thebook/ch5-structs/src/main.rs
+++ thebook/ch5-structs/src/main.rs
@@ -28,6 +28,40 @@ fn main() {
         height: 45,
     };
 
+    println!("rect1 area: {}", rect1.area());
     println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
     println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn larger_can_hold_smaller() {
+        let larger = Rectangle {
+            width: 8,
+            height: 7,
+        };
+        let smaller = Rectangle {
+            width: 5,
+            height: 1,
+        };
+
+        assert!(larger.can_hold(&smaller));
+    }
+
+    #[test]
+    fn smaller_cannot_hold_larger() {
+        let larger = Rectangle {
+            width: 8,
+            height: 7,
+        };
+        let smaller = Rectangle {
+            width: 5,
+            height: 1,
+        };
+
+        assert!(!smaller.can_hold(&larger));
+    }
 }
